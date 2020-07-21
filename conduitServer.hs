1f04a185bb973be54dbc7e83b3c46108a9899b7b
Default system user <beb82@cornell.edu>
2020-07-21 22:06:18 +0000

    deleting cruft

--- conduitServer.hs
+++ conduitServer.hs
@@ -20,12 +20,20 @@
 {-# LANGUAGE RankNTypes                  #-}
 {-# LANGUAGE ScopedTypeVariables         #-}
 
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE PackageImports #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+
 module Main where
 
 import           ClassyPrelude                    hiding (hClose)
 import           Conduit
 import           Control.Concurrent.STM.TBQueue   (TBQueue, writeTBQueue)
 import           Control.Monad.Catch              (MonadMask)
+import           Control.Monad.Loops
 import           Control.Monad.Writer
 import           Data.Bits                        (shiftR, (.&.))
 import qualified Data.ByteString.Char8            as B
@@ -36,6 +44,7 @@ import qualified Data.Text                        as T
 import           GHC.IO.Handle                    (Handle, hClose)
 import qualified Network.Simple.TCP               as TCP
 import qualified Network.Socket                   as NS
+import qualified System.IO                        as IO
 import           UnliftIO.Concurrent              (ThreadId, forkIO, threadDelay)
 
 type Error = [String]
@@ -52,6 +61,28 @@ waitForever = do
   threadDelay 10000
   waitForever
 
+
+-- | Wraps multiple calls to decodeMessageDelimitedH; instead of returning a list, we
+-- | maintain the type signature for simplicity.
+decodeMessages :: Handle -> IO (Either String String)
+decodeMessages h = do
+  putStrLn $ T.pack "starting decodeMessages"
+  msgs <- go ""
+  putStrLn $ T.pack "finishing decodeMessages"
+  pure msgs
+  where
+    go :: String -> IO (Either String String)
+    go xs = do
+      isR <- IO.hReady h
+      case isR of
+        False -> do
+          putStrLn $ T.pack "finishing go"
+          pure $ Right xs
+        True -> do
+          sOut <- B.unpack <$> B.hGetLine h
+          putStrLn $ T.pack $ "looping in go: " <> sOut <> xs
+          go (sOut <> xs)
+      
 -- | This signature is meant to simulate the same function from the proto-lens library,
 -- | but without dealing with protobus for binary data.
 decodeMessageDelimitedH :: Handle -> IO (Either String String)
@@ -68,12 +99,12 @@ protoServe fromProto = start .| mapMC logFilterRead
     port = trace "getting protobuf port" getPort
     start = do
       let enQserver = serveTBQ (TCP.HostIPv4) port (decodeProto . fst)
-      gatherFrom 10000 enQserver
+      mygatherFrom 10000 enQserver
     decodeProto :: NS.Socket -> m (Either String String)
     decodeProto sock = bracket
       connHandleIO
       (liftIO . hClose)
-      (liftIO . decodeMessageDelimitedH)
+      (liftIO . decodeMessages)
       where
         connHandleIO :: m Handle
         connHandleIO = liftIO $ sockToHandle sock
@@ -84,7 +115,7 @@ protoServe fromProto = start .| mapMC logFilterRead
     msgToRecs :: String -> m [String]
     msgToRecs p = case runResult $ fromProto p of
       (rs, rErr) -> do
-        when (not $ null rErr) $ pure $ trace (intercalate "\n" rErr) ()
+        when (not $ null rErr) $ pure $ trace (intercalate " " rErr) ()
         pure $ trace "completed msgToRecs" rs
 
 -- | The handle only needs a read-view of the socket.  Note that a TBQeueue is
@@ -124,10 +155,41 @@ retryForever prog = catchAny prog progRetry
 sinkUnits :: MonadResource m => ConduitT () Void m ()
 sinkUnits = sinkNull
 
+
+-- | Gather output values asynchronously from an action in the base monad and
+--   then yield them downstream.  This provides a means of working around the
+--   restriction that 'ConduitM' cannot be an instance of 'MonadBaseControl'
+--   in order to, for example, yield values from within a Haskell callback
+--   function called from a C library.
+mygatherFrom :: forall m o. (MonadIO m, MonadUnliftIO m)
+           => Int                -- ^ Size of the queue to create
+           -> (TBQueue o -> m ()) -- ^ Action that generates output values
+           -> ConduitT () o m ()
+mygatherFrom size scatter = do
+    liftIO $ putStrLn $ T.pack "entering gatherFrom"
+    chan   <- liftIO $ newTBQueueIO (fromIntegral size)
+    worker <- lift $ async (scatter chan)
+    res <- gather worker chan
+    liftIO $ putStrLn $ T.pack "leaving gatherFrom"
+    pure res
+  where
+    gather :: Async b -> TBQueue o -> ConduitT i o m b
+    gather worker chan = do
+        (xs :: [o], mres) <- liftIO $ atomically $ do
+            xs <- whileM (not <$> isEmptyTBQueue chan) (readTBQueue chan)
+            (xs,) <$> pollSTM worker
+        -- instead of doing traverse_, we can do one yield at a time
+        traverse_ yield xs
+        case mres of
+            Just (Left e)  -> liftIO $ throwIO (e :: SomeException)
+            -- Just (Right r) -> return r
+            _                -> gather worker chan
+
 main :: IO ()
 main = retryForever $ do
   putStrLn $ T.pack "starting tcp server"
   let myProtoServe = protoServe (pure . words)
-  myProtoServe .| mapMC (putStrLn . T.pack . intercalate "_") .| sinkUnits & runConduitRes
-  putStrLn $ T.pack "tcp server exited"
+  xs <- myProtoServe .| mapMC (putStrLn . T.pack . (":: " <> ) . intercalate "_") .| sinkUnits & runConduitRes
+  -- this next line will never be reached now:
+  putStrLn $ T.pack $ "tcp server exited with value: " <> (show xs) 
   waitForever
